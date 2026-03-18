3aff3da57a6ba540665bb70998ae4c660c5e9c3f
Anubhav Jha <anubhavjha424@gmail.com>
2026-03-18 12:00:02 +0530

    index file done

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 export default {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
->>>>>>> a00300cec0f24492c26a1da5dff1658cf44aeed9
